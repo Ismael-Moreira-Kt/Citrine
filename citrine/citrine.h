f7389f9c160deadede918e48257bf28ae70fc80e
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-29 22:48:30 +0100

    feat: Addition of the interface that simplifies writing C files.

--- citrine/citrine.h
+++ citrine/citrine.h
@@ -18,6 +18,7 @@ ssize_t write_file_asm(int fd, const void *buffer, size_t count);
 int create_file(const char *path, mode_t mode);
 int open_file(const char *path, int flags);
 ssize_t read_file(int fd, void *buffer, size_t count);
+ssize_t write_file(int fd, const void *buffer, size_t count);
 
 
 
