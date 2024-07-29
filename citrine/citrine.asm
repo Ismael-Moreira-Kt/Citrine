bdf0660a128fa6e0014d63eb903aa9a88b99b659
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-29 22:36:31 +0100

    feat: Addition of the function that makes the syscall to read files.

--- citrine/citrine.asm
+++ citrine/citrine.asm
@@ -4,9 +4,16 @@ section .note.GNU-stack noalloc noexec nowrite progbits
 
 section .text
     global open_file_asm
+    global read_file_asm
 
 
 open_file_asm:
     mov rax, 2
+    syscall
+    ret
+
+
+read_file_asm:
+    mov rax, 0
     syscall
     ret
