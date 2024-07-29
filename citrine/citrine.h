2c743c8a7e5edc367bf753d479cecf6dc5ae837f
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-29 22:37:57 +0100

    feat: Addition of the function insertion in the header.

--- citrine/citrine.h
+++ citrine/citrine.h
@@ -8,11 +8,13 @@
 
 
 int open_file_asm(const char *path, int flags, mode_t mode);
+ssize_t read_file_asm(int fd, void *buffer, size_t count);
 
 
 
 int create_file(const char *path, mode_t mode);
 int open_file(const char *path, int flags);
+
 
 
 #endif
