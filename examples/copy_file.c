a87a2e52d117b740ea02a636e404488b66b47c94
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-30 22:21:03 +0100

    docs: Creation of an example of using the copyFile function.

---
+++ examples/copy_file.c
@@ -0,0 +1,24 @@
+#include "examples.h"
+
+
+
+void copyFile() {
+    const char *source_path = "example.txt";
+    const char *destination_path = "copy_of_example.txt";
+
+    int result = copy_file(source_path, destination_path);
+    
+    if (result == 0) {
+        printf("File copied successfully.\n");
+        
+        mode_t permissions = PERM_RW_OWNER;
+
+        if (set_file_permissions(destination_path, permissions) == 0) {
+            printf("Permissions set successfully on copied file.\n");
+        } else {
+            printf("Error setting permissions on copied file.\n");
+        }
+    } else {
+        printf("Error copying file.\n");
+    }
+}
