187fe0cebf4d66b4e68203757032152ffdf1cd40
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-30 13:21:59 +0100

    feat: Interface implementation of the function that checks whether a file exists.

--- citrine/citrine.c
+++ citrine/citrine.c
@@ -2,24 +2,28 @@
 
 
 
-extern int open_file_asm(const char *path, int flags, mode_t mode);
-extern ssize_t read_file_asm(int fd, void *buffer, size_t count);
-extern ssize_t write_file_asm(int fd, const void *buffer, size_t count);
-extern int close_file_asm(int fd);
-extern int set_permissions_asm(const char *path, mode_t mode);
+extern open_file_asm(const char *path, int flags, mode_t mode);
+extern read_file_asm(int fd, void *buffer, size_t count);
+extern write_file_asm(int fd, const void *buffer, size_t count);
+extern close_file_asm(int fd);
+extern set_permissions_asm(const char *path, mode_t mode);
+extern unlink_file_asm(const char *path);
+extern access_file_asm(const char *path, int mode);
+extern fstat_file_asm(int fd, struct stat *statbuf);
+extern mkdir_asm(const char *pathname, mode_t mode);
 
 
 
-CitrineFile open_ile(const char *path, int flags, mode_t mode) {
+CitrineFile open_or_create_file(const char *path, int flags, mode_t mode) {
     CitrineFile file;
-    
+
     file.fd = open_file_asm(path, flags, mode);
     file.path = path;
-    
+
     if (file.fd == -1) {
         perror("Error opening or creating file");
     }
-    
+
     return file;
 }
 
@@ -27,11 +31,11 @@ CitrineFile open_ile(const char *path, int flags, mode_t mode) {
 
 ssize_t read_from_file(CitrineFile *file, void *buffer, size_t count) {
     ssize_t bytesRead = read_file_asm(file->fd, buffer, count);
-    
+
     if (bytesRead == -1) {
         perror("Error reading file");
     }
-    
+
     return bytesRead;
 }
 
@@ -39,11 +43,11 @@ ssize_t read_from_file(CitrineFile *file, void *buffer, size_t count) {
 
 ssize_t write_to_file(CitrineFile *file, const void *buffer, size_t count) {
     ssize_t bytesWritten = write_file_asm(file->fd, buffer, count);
-    
+
     if (bytesWritten == -1) {
         perror("Error writing file");
     }
-    
+
     return bytesWritten;
 }
 
@@ -51,11 +55,11 @@ ssize_t write_to_file(CitrineFile *file, const void *buffer, size_t count) {
 
 int close_file(CitrineFile *file) {
     int result = close_file_asm(file->fd);
-    
+
     if (result == -1) {
         perror("Error closing file");
     }
-    
+
     return result;
 }
 
@@ -63,10 +67,16 @@ int close_file(CitrineFile *file) {
 
 int set_file_permissions(const char *path, mode_t mode) {
     int result = set_permissions_asm(path, mode);
-    
+
     if (result == -1) {
         perror("Error setting permissions");
     }
-    
+
     return result;
+}
+
+
+
+int file_exists(const char *path) {
+    return (access_file_asm(path, F_OK) != -1);
 }
