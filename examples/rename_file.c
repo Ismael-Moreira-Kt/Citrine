c4cf148857cf8427cf0b4c5bd33242a87c1702f4
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-31 20:27:34 +0100

    refactor: Modification of the example of using the renameFile function.

--- examples/rename_file.c
+++ examples/rename_file.c
@@ -2,8 +2,8 @@
 
 
 
-void renameFile() {
-    int result = rename_file("example.txt", "new_name.txt");
+void _renameFile() {
+    int result = renameFile("example.txt", "new_name.txt");
     
     if (result == 0) {
         printf("File renamed successfully.\n");
