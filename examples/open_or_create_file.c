1a970ca092043246820b224394ee2814019fd8cf
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-31 20:22:52 +0100

    refactor: Modification of the example of using the openFile function.

--- examples/open_or_create_file.c
+++ examples/open_or_create_file.c
@@ -2,8 +2,8 @@
 
 
 
-void openOrCreateFile() {
-    CitrineFile file = open_or_create_file("example.txt", O_RDWR | O_CREAT, PERM_RW_OWNER);
+void _openOrCreateFile() {
+    CitrineFile file = openFile("example.txt", O_RDWR | O_CREAT, PERM_RW_OWNER);
     
     if (file.fd != -1) {
         printf("File opened or created successfully.\n");
