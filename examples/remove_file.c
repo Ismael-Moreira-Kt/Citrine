5c7eba7b6852445c5f02dd48766364587a6960ec
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-31 20:27:02 +0100

    refactor: Modification of the example of using the removeFile function.

--- examples/remove_file.c
+++ examples/remove_file.c
@@ -2,9 +2,9 @@
 
 
 
-void removeFile() {
-    CitrineFile file = open_or_create_file("example.txt", O_RDWR | O_CREAT, PERM_RW_OWNER);
-    int result = remove_file("example.txt");
+void _removeFile() {
+    CitrineFile file = openFile("example.txt", O_RDWR | O_CREAT, PERM_RW_OWNER);
+    int result = removeFile("example.txt");
     
     if (result == 0) {
         printf("File removed successfully.\n");
