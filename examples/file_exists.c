72a2d59a33492e5950aebc8a194976f3e99732ae
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-31 20:17:15 +0100

    refactor: Modification of the example of using the fileExists function.

--- examples/file_exists.c
+++ examples/file_exists.c
@@ -2,8 +2,8 @@
 
 
 
-void fileExists() {
-    if (file_exists("example.txt")) {
+void _fileExists() {
+    if (fileExists("example.txt")) {
         printf("File exists.\n");
     } else {
         printf("File does not exist.\n");
