c989b06cbbc756a0728f2c9d08b7c10b46be1390
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-31 19:52:49 +0100

    refactor: Modification of the example of using the changeOwner function.

--- examples/change_file_owner.c
+++ examples/change_file_owner.c
@@ -2,9 +2,9 @@
 
 
 
-void changeFileOwner() {
-    int result = change_file_owner("example.txt", getuid(), getgid());
-    
+void _changeFileOwner() {
+    int result = changeOwner("example.txt", getuid(), getgid());
+
     if (result == 0) {
         printf("File owner changed successfully.\n");
     }
